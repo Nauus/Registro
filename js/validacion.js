ed4f509b209ecaa86352fc8abf8edc4d7a569236
Nauu' s <nahuelalonsouy@gmail.com>
2023-08-10 08:35:21 -0300

    alerts

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,25 @@
+document.getElementById("regBtn").addEventListener("click", function () {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let terminos = document.getElementById("terminos").value;
+
+    if (nombre === "" || apellido === "" || password1 === "" || password2 === "" || !terminos) {
+        showAlertError()
+    } else if (password1.length < 6) {
+        showAlertError()
+    } else if (password1 !== password2) {
+        showAlertError()
+} else {
+    showAlertSuccess()
+}
+});
+
+
+
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
